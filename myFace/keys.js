fd1a07f46a3ffe4a4d47170755b024c0dad86578
sodizdaroglu <americanlaz@gmail.com>
2020-07-27 17:25:59 -0400

    first commit

---
+++ myFace/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    MONGOURI :"mongodb+srv://americanlaz:iivVzT9WFgtQdHaX@cluster0.n3okc.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
---
+++ myFace/routes/auth.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require ('mongoose')
+const User = mongoose.model('User')
+const bcrypt = require('bcryptjs')
+
+
+
+router.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+
+router.post('/signup',(req,res)=>{
+  
+    const {name,email,password,pic} = req.body 
+    if(!email || !password || !name){
+       return res.status(422).json({error:"please add all the fields"})
+      }
+      User.findOne({email:email})
+  .then((savedUser)=>{
+      if(savedUser){
+        return res.status(422).json({error:"user already exists with that email"})
+      }
+      // hashing a password, biger the number more secure the password
+      bcrypt.hash(password,12)
+      .then(hashedpassword=>{
+            const user = new User({
+                email,
+                password:hashedpassword,
+                name,
+                pic
+            })
+    
+            user.save()
+            .then(user=>{
+                // transporter.sendMail({
+                //     to:user.email,
+                //     from:"no-reply@insta.com",
+                //     subject:"signup success",
+                //     html:"<h1>welcome to instagram</h1>"
+                // })
+                res.json({message:"saved successfully"})
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+      })
+     
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+})
+
+ 
+
+module.exports = router
