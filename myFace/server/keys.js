1a56c4b7da6b5707fd9eb54d5f65a5474c83829b
sodizdaroglu <americanlaz@gmail.com>
2020-07-30 23:57:09 -0400

    upload pic

--- myFace/server/keys.js
+++
@@ -1,6 +0,0 @@
-const { JWT_SECRET } = require("../Instagram-clone-MERN-Stack-master/config/prod");
-
-module.exports ={
-    MONGOURI :"mongodb+srv://americanlaz:iivVzT9WFgtQdHaX@cluster0.n3okc.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET:"dhsdjklfghsdla678lfkaldskf"
-}
